c85b1144db4bb274acd0e51a6a79ba51770503cc
jeomiah <ehdaorwn3192@gmail.com>
2022-05-12 14:54:39 +0900

    First commit

---
+++ pkg/render/render.go
@@ -0,0 +1,17 @@
+package render
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+// RenderTemplate renders templates using html/template
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parseTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+}
